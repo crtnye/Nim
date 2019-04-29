b53adfb057eee6cf527b3a4c1331596fc0acb1da
calbers1 <calbers1@harding.edu>
2019-04-29 14:55:48 -0500

    .

--- Nim/playNim.cpp
+++ Nim/playNim.cpp
@@ -12,33 +12,38 @@
 using namespace std;
 
 //Returns the initial pile configuration 
-char * initializeBoard(Piles &piles)
+string initializeBoard(int piles[])
 {
+<<<<<<< HEAD
 	
 	srand(2);
+=======
+	srand(time(0));
+>>>>>>> parent of 2b34351... Merge branch 'master' of https://github.com/crtnye/Nim
 	int maxPiles = rand() % 9 + 3;
-	piles.numPiles = maxPiles;
 	char t[3];
 	itoa(maxPiles, t, 10);
-	strcat(piles.board, t);
+	string temp;
+	temp.append(t);
 
 	for (int i = 0; i < maxPiles; i++) {
-		piles.pile[i] = rand() % 20 + 1;
-		int x = piles.pile[i];
+		//piles[i] = rand() % 20 + 1;
+		//int x = piles[i];
+		int x = rand() % 20 + 1;
+		piles[i] = x;
 		itoa(x, t, 10);
-		if (piles.pile[i] < 10) {
-			strcat(piles.board, "0");
-			strcat(piles.board, t);
+		if (piles[i] < 10) {
+			temp.append("0");
+			temp.append(t);
 		}
 		else {
-
-			strcat(piles.board, t);
+			temp.append(t);
 		}
 	}
-	return piles.board;
+	return temp;
 }
 
-void updateBoard(Piles &piles, string move, int Player)
+void updateBoard(int piles[], string move, int Player)
 {
 	//TODO: Update the piles array to reflect the move
 	istringstream iss(move.substr(0,1));
@@ -49,40 +54,45 @@ void updateBoard(Piles &piles, string move, int Player)
 	istringstream iss2(move.substr(1, 2));
 	iss2 >> r;
 
-	piles.pile[p] -= r;
+	piles[p] -= r;
 
 }
 
-void displayBoard(Piles &piles)
+void displayBoard(int piles[])
 {
 	//TODO: Display the game board
-
-	for (int i = 0; i < piles.numPiles; i++) {
-		cout << "pile " << i + 1 << ": ";
-		for (int f = 0; f < piles.pile[i]; f++) {
+	for (int i = 0; i < *piles; i++) {
+		cout << "PILE " << i << ": ";
+		for (int f = 0; f < piles[i]; i++) {
 			cout << "*";
 		}
-		cout << right << setw(13) << "pile " << i + 1 << ": " << piles.pile[i] << endl;
+		
+		cout << right << setw(12) << "PILE " << i << ": " << piles[i] << endl;
 	}
 }
 
-int check4Win(Piles &piles)
+int check4Win(int piles[])
 {
 	//TODO: Check to see if the game is over
 	//Notify the player if the game is over and who won.
 	return 0;
 }
 
-string getMove(const Piles &piles, int Player)
+char* getMove(const int piles[], int Player)
 {
 	//TODO:
 	//Ask the player for their move
 	//Ask for pile, then number of rocks to remove (make sure both are valid input)
 	//return the move as a string
 
+	char command[36];
 
-	string temp = "";
-	return temp;
+	cout << "It's your turn!" << endl;
+	cout << "Enter first letter of one of the following commands (C,F, or R)."
+		<< "Command(Chat, Forfeit, Remove - rocks) ? ";
+	cin >> command;
+	
+	return command;
 }
 
 int playNim(SOCKET s, string serverName, string remoteIP, string remotePort, int localPlayer)
@@ -91,8 +101,7 @@ int playNim(SOCKET s, string serverName, string remoteIP, string remotePort, int
 	// will be one of the following values: noWinner, CWinner, PWinner, ABORT
 	int winner = noWinner;
 	string initialBoardConfig; //Contains the initial mnnnnnnnn format
-	Piles piles;
-	piles.pile; //Keeps track of the piles during gameplay. piles[0] contains the number of rocks the first pile has left.
+	int piles[9] = {0, 0, 0, 0, 0, 0, 0, 0, 0}; //Keeps track of the piles during gameplay. piles[0] contains the number of rocks the first pile has left.
 	int opponent;
 	string move;
 	bool myMove;
@@ -105,6 +114,7 @@ int playNim(SOCKET s, string serverName, string remoteIP, string remotePort, int
 		myMove = false;
 		initialBoardConfig = initializeBoard(piles);
 
+
 		//TODO: Send intialBoardConfig to the server using UDP_send
 
 
@@ -116,10 +126,38 @@ int playNim(SOCKET s, string serverName, string remoteIP, string remotePort, int
 		if (myMove) {
 			// Get my move & display board
 			move = getMove(piles, localPlayer);
-			//TODO: Some checks on the move the player wants to make
-			//Did the player forfeit?
-			//Is the player just sending a comment?
-			//Is the move they sent invalid?
+			char message[MAX_SEND_BUF];
+
+			if (move[0] == 'C') {
+				char comment[80];
+				comment[0] = 'C';
+				char input[79];
+				cout << endl << "Comment?";
+				cin >> input;
+				strcat(comment, input);
+				strcpy(message, comment);
+			}
+			else if (move[0] == 'F') {
+
+				cout << endl << "You forfeited, " << opponent << " won." << endl;
+				strcpy(message, (char*)'F');
+			}
+			else if (move[0] == 'R') {
+				int pile;
+				int numRocks;
+				cout << "Which pile? ";
+				cin >> pile;
+				cout << endl << "How many rocks?";
+				cin >> numRocks;
+
+				//was the move valid? if so...
+				strcpy(message, (char*)pile);
+				strcat(message, (char*)numRocks);
+			}
+			else {
+				//not sure what goes here
+			}
+
 
 			cout << "Board after your move:" << endl;
 
@@ -127,7 +165,6 @@ int playNim(SOCKET s, string serverName, string remoteIP, string remotePort, int
 			displayBoard(piles);
 
 			// Send move to opponent
-
 			char mutableMove[4];
 			strcpy(mutableMove, move.c_str());
 
